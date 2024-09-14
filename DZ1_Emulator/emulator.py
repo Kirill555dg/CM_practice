534d55eebac26344fca899052c80297377f629de
Kerrodar <Kirill555dg@gmail.com>
2024-09-14 23:13:57 +0300

    update DZ1

--- DZ1_Emulator/emulator.py
+++ DZ1_Emulator/emulator.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 import tarfile
 import os.path
 import csv
@@ -6,6 +7,9 @@ import xml
 from os.path import exists
 
 from sys import argv
+
+from DZ1_Emulator.terminal import Terminal
+from terminal import Terminal
 
 archive_name = 'archive.tar'
 
@@ -37,7 +41,7 @@ def read_tar_archive():
             f.getnames()
 
 
-def execute():
+def read_config_file():
     if len(argv) < 2:
         print("Для корректного запуска эмулятора необходимо ввести: " 
               "\"py emulator.py <путь к конфигурационному файлу>\""
@@ -45,21 +49,61 @@ def execute():
               "\narchive_path,<путь к архиву>"
               "\nlog_file_path,<путь к лог-файлу>"
               "\nstart_script_path,<путь к стартовому скрипту>")
-        return
+        exit(1)
 
     config_file_path = argv[1]
     if not exists(config_file_path):
         print("Конфигурационный файл по заданному пути не найден.")
-        return
+        exit(1)
     if not config_file_path.endswith(".csv"):
         print("Указанный конфигурационный файл задан в неверном формате.")
-        return
+        exit(1)
 
     with open(config_file_path, newline='') as config_file:
         config_reader = csv.reader(config_file, delimiter=",", quotechar="|")
+        names = ['archive_path', 'log_file_path', 'start_script_path']
+        archive_path, log_file_path, start_script_path = None, None, None
         for row in config_reader:
-            print(row)
+            name = row[0]
+            path = row[1]
+            if not exists(path):
+                print(f"Файл для {name} не найден.")
+                exit(1)
+            if name == "archive_path":
+                if not tarfile.is_tarfile(path):
+                    print("Для archive_path указан файл, не являющийся tar-архивом.")
+                    exit(1)
+                archive_path = path
+            if name == "log_file_path":
+                if not path.endswith(".xml"):
+                    print("Для log_file_path указан неверный формат файла. Файл должен быть формата xml.")
+                    exit(1)
+                log_file_path = path
+            if name == "start_script_path":
+                start_script_path = path
+            if name not in names:
+                print("В конфигурационном файле присутствует неопознанный ключ: " + name)
 
+        is_exit = False
+        if archive_path is None:
+            print("В конфигурационном файле не указан путь для tar-архива")
+            is_exit = True
+        if log_file_path is None:
+            print("В конфигурационном файле не указан путь для лог-файла")
+            is_exit = True
+        if start_script_path is None:
+            print("В конфигурационном файле не указан путь для стартового скрипта")
+            is_exit = True
+
+        if is_exit:
+            exit(1)
+
+        return archive_path,log_file_path,start_script_path
+
+def execute_terminal(paths):
+    terminal = Terminal(paths)
 
 if __name__ == "__main__":
-    execute()
+    paths = read_config_file()
+    execute_terminal(paths)
+
--- DZ1_Emulator/terminal.py
+++ DZ1_Emulator/terminal.py
@@ -1,8 +1,43 @@
 import tarfile
+import xml.etree.ElementTree
 
 class Terminal:
+    def __init__(self, archive_path, log_file_path, start_script_path):
+        self.archive_path = archive_path
+        self.log_file_path = log_file_path
+        self.start_script_path = start_script_path
+        self.current_directory = ''
+        self.running = False
 
+    def run(self):
+        self.running = True
+        while self.running:
 
+            return
 
-    def __init__(self, archive: tarfile.TarFile, log_file, start_script):
-        self.archive = archive
+    def execute_start_script(self):
+        with open(self.start_script_path, 'r') as start_script:
+            for command in start_script.readlines():
+                if len(command) > 0:
+                    self.parse_cmd(command.strip())
+
+    def parse_cmd(self, command):
+        params = command.split()
+        if params[0] == 'exit':
+            self.running = False
+            return
+
+    def ls(self):
+        pass
+
+    def cd(self):
+        pass
+
+    def rev(self):
+        pass
+
+    def find(self):
+        pass
+
+    def date(self):
+        pass
